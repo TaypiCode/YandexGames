82792a75792cf2783c9b64904345e521ab42805d
TaypiCode <taypi.code@gmail.com>
2022-12-28 14:07:30 +0300

    save

---
+++ Scripts/OnCloseScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCloseScript : MonoBehaviour
+{
+    private SaveGame _save;
+    private void Start()
+    {
+        _save = FindObjectOfType<SaveGame>();
+    }
+    public void OnClose()
+    {
+        _save.SaveProgress();
+    }
+}
